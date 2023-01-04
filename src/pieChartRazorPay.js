f46f2ed48e85d655d950aae645795beacf016580
ParasArya289 <parasarya289@gmail.com>
2023-01-04 22:27:38 +0530

    razorPay interview question

---
+++ src/pieChartRazorPay.js
@@ -0,0 +1,22 @@
+//RazorPay interview question
+
+import { useState } from "react";
+
+let PieChart = () =>{
+  let [value,setValue] = useState(0);
+  // console.log(value);
+  return (
+    <>
+    <input type="range" onChange={e=>setValue(e.target.value)}/>
+    <div style={{
+      width:'200px',
+      height:'200px',
+      margin:'auto',
+      backgroundImage:`conic-gradient(#7c3aed,${value}%,#a78bfa 0)`,
+      borderRadius:'50%',
+      border:'2px solid grey'
+    }}></div>
+    </>
+  )
+}
+export default PieChart;

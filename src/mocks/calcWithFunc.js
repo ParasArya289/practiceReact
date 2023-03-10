9828024da5ff086a33d61369f632ed08856313ce
Paras Arya <parasarya289@gmail.com>
2023-03-10 23:59:29 +0530

    calcWithFunc

--- src/mocks/calcWithFunc.js
+++ src/mocks/calcWithFunc.js
@@ -1,17 +1,18 @@
-const zero = (a) => (a ? `${0}${a}` : 0);
-const one = (a) => (a ? `${1}${a}` : 1);
-const two = (a) => (a ? `${2}${a}` : 2);
-const three = (a) => (a ? `${3}${a}` : 3);
-const four = (a) => (a ? `${4}${a}` : 4);
-const five = (a) => (a ? `${5}${a}` : 5);
-const six = (a) => (a ? `${6}${a}` : 6);
-const seven = (a) => (a ? `${7}${a}` : 7);
-const eight = (a) => (a ? `${8}${a}` : 8);
-const nine = (a) => (a ? `${9}${a}` : 9);
+const zero = (a) => a?Math.trunc(eval(`${0}${a}`)):0;
+const one  = (a) => a?Math.trunc(eval(`${1}${a}`)):1;
+const two  = (a) => a?Math.trunc(eval(`${2}${a}`)):2;
+const three  = (a) => a?Math.trunc(eval(`${3}${a}`)):3;
+const four  = (a) => a?Math.trunc(eval(`${4}${a}`)):4;
+const five  = (a) => a?Math.trunc(eval(`${5}${a}`)):5;
+const six  = (a) => a?Math.trunc(eval(`${6}${a}`)):6;
+const seven  = (a) => a?Math.trunc(eval(`${7}${a}`)):7;
+const eight  = (a) => a?Math.trunc(eval(`${8}${a}`)):8;
+const nine  = (a) => a?Math.trunc(eval(`${9}${a}`)):9;
 
 const plus = (a) => `+${a}`;
-const minus = (a) => `-${a}`;
+const minus = (a) =>`-${a}`;
 const dividedBy = (a) => `/${a}`;
 const times = (a) => `*${a}`;
 
 console.log(eval(seven(times(seven()))));
+// 7*7 = 49

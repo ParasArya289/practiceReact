4e5828b1b3b0a3a28a8fce821b4f8c1b63e66bf4
Paras Arya <parasarya289@gmail.com>
2023-03-16 23:51:27 +0530

    dirReduc

--- src/mocks/dirReduction.js
+++ src/mocks/dirReduction.js
@@ -1,12 +1,16 @@
 function dirReduc(arr) {
-  const map = new Map();
-  arr.forEach((el) => map.set(el, (map.get(el) || 0) + 1));
-  console.log(map);
   const wrongWay = {
     NORTH: "SOUTH",
     SOUTH: "NORTH",
     EAST: "WEST",
     WEST: "EAST",
   };
-  return map.has(wrongWay["NORTH"]);
+
+  return arr.reduce(
+    (acc, dir) =>
+      wrongWay[dir] === acc[acc.length - 1]
+        ? acc.slice(0, -1)
+        : acc.concat(dir),
+    []
+  );
 }

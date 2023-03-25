3f9145a3ac92f1f70256e4cfbd1d9876a5f3618a
Paras Arya <parasarya289@gmail.com>
2023-03-26 01:27:05 +0530

    dfsRecursive

--- src/mocks/dfsTraversal.js
+++ src/mocks/dfsTraversal.js
@@ -1,32 +1,39 @@
-function Node(val){
+function Node(val) {
   this.root = val;
   this.left = null;
   this.right = null;
 }
 
-let a = new Node('a');
-let b = new Node('b');
-let c = new Node('c');
-let d = new Node('d');
-let e = new Node('e');
-let f = new Node('f');
+let a = new Node("a");
+let b = new Node("b");
+let c = new Node("c");
+let d = new Node("d");
+let e = new Node("e");
+let f = new Node("f");
 a.left = b;
 a.right = c;
 b.left = d;
 b.right = e;
 c.right = f;
 
-
-const dfs = function(root){
+const dfs = function (root) {
   const list = [];
-  const stack = [root]
-  while(stack.length>0){
+  const stack = [root];
+  while (stack.length > 0) {
     const curr = stack.pop();
     list.push(curr.root);
-    if(curr.right) stack.push(curr.right);
-    if(curr.left) stack.push(curr.left)
+    if (curr.right) stack.push(curr.right);
+    if (curr.left) stack.push(curr.left);
   }
   return list;
-}
+};
 
-console.log(dfs(a))
+const dfsRecursive = function (root) {
+  if (root === null) return [];
+  const leftSubtree = dfsRecursive(root.left);
+  const rightSubtree = dfsRecursive(root.right);
+  console.log({ leftSubtree, rightSubtree });
+  return [root.root, ...leftSubtree, ...rightSubtree];
+};
+
+// console.log(dfs(a))
